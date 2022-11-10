589969ea65652b5dc61cad7fea168f75b0a61ffc
X00185002 Sayali Mote <X00185002@TA.IT-TALLAGHT.IE>
2022-11-10 12:43:05 +0000

    4th commit

--- books/views.py
+++ books/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.views.generic import ListView, BookDetailView
+from .models import Book
+
 
 # Create your views here.
+class BookListView(ListView):
+    model = Book
+    context_object_name = 'book_list'
+    template_name = 'books/book_list.html'
+
+class BookListView(ListView):
+    model = Book
+    
